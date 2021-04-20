9d3fd0b2672a31691a8879e3621f7454cab65739
Jose Jorge Rodriguez Salgado <josejorgexl@gmail.com>
2021-04-19 23:32:02 -0400

    ref: change cleaner test file

--- backend/test/cleaner.js
+++ backend/test/cleaner.js
@@ -2,23 +2,14 @@ import mongoose from "mongoose"
 import { connection } from "../app.js"
 import MongooseConnection from "../config/mongoose.js"
 
-var connector = new MongooseConnection()
-connector.getConnection()
-var conn = connector.connection
-
 before((done) => {
-    conn.collections.users.drop(() => {
+    var connector = new MongooseConnection()
+    connector.getConnection()
+    connector.connection.collections.users.drop(() => {
         done()
     })
 })
 
 after((done) => {
-    // connection.close(() => {
-    //     conn.close(() => {
-    //         console.log("CONNECTION CLOSED");
-    //         done()
-    //     })
-    // })
-
     mongoose.disconnect(done)
 })
