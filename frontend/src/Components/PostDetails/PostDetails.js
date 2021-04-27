f50a96deb56ec74aff699c57019994c8e6669427
Amalia Ibarra <amalia.ibarra@estudiantes.matcom.uh.cu>
2021-04-27 11:55:10 -0400

    feat: Add Card component

--- frontend/src/Components/PostDetails/PostDetails.js
+++ frontend/src/Components/PostDetails/PostDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Card from "../Layout/Card/Card"
 
 
 class PostDetails extends Component {
@@ -9,25 +10,21 @@ class PostDetails extends Component {
         fetch("https://jsonplaceholder.typicode.com/todos/" + this.props.match.params.id)
             .then(response => response.json())
             .then(data => { this.setState({ post: data }) })
+            .catch(error => console.log(error))
     }
     render() {
         const post = { ...this.state.post }
         return (
-            <div className="mx-auto col-10 col-lg-6 mt-3 mb-3" >
-                <div className="Post card">
-                    <div className="card-body">
-                        <div className="d-flex flex-column flex-lg-row align-items-center">
-                            <img src={post.path} className="img-fluid rounded col-12 col-lg-6" alt="" />
-                            <small>{post.title}</small>
-                            <div className="d-flex flex-column col-12 col-lg-6 align-items-center">
-                                <small className="mr-3 ml-3">Sexo: {post.sex}</small>
-                                <small>Edad: {post.age}</small>
-                            </div>
-                        </div>
-
+            <Card color="lightblue">
+                <div className="d-flex flex-column flex-lg-row align-items-center">
+                    <img src={post.path} className="img-fluid rounded col-12 col-lg-6" alt="" />
+                    <small>{post.title}</small>
+                    <div className="d-flex flex-column col-12 col-lg-6 align-items-center">
+                        <small className="mr-3 ml-3">Sexo: {post.sex}</small>
+                        <small>Edad: {post.age}</small>
                     </div>
                 </div>
-            </div >
+            </Card >
 
         )
     }
--- frontend/src/Components/Posts/Post/Post.js
+++ frontend/src/Components/Posts/Post/Post.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Post.css"
+import Card from "../../Layout/Card/Card"
 
 const post = (props) => {
     return (
-        <div className="mx-auto col-10 col-lg-6 mt-3 mb-3" >
-            <div className="Post card">
-                <div className="card-body">
-                    <div className="d-flex flex-column flex-lg-row align-items-center">
-                        <img src={props.path} className="img-fluid rounded col-12 col-lg-6" alt="" />
-                        <small>{props.title}</small>
-                        <div className="d-flex flex-column col-12 col-lg-6 align-items-center">
-                            <small className="mr-3 ml-3">Sexo: {props.sex}</small>
-                            <small>Edad: {props.age}</small>
-                            <div className="d-flex flex-row mt-5">
-                                <Link to={"/post-details/" + props.id} className="ml-auto" type="button">Detalles</Link>
-                            </div>
-                        </div>
-                    </div>
-
+        <Card color="lightblue">
+            <div className="d-flex flex-column flex-lg-row align-items-center">
+                <img src={props.path} className="img-fluid rounded col-12 col-lg-6" alt="" />
+                <small>{props.title}</small>
+                <div className="d-flex flex-column col-12 col-lg-6 align-items-center">
+                    <small className="mr-3 ml-3">Sexo: {props.sex}</small>
+                </div >
+                <small>Edad: {props.age}</small>
+                <div className="d-flex flex-row mt-5">
+                    <Link to={"/post-details/" + props.id} className="ml-auto" type="button">Detalles</Link>
                 </div>
             </div>
-        </div >
+        </Card >
     );
 }
 
