b1159d3b0bbe206584761b734abbfc14f3a50ae7
Amalia Ibarra <amalia.ibarra@estudiantes.matcom.uh.cu>
2021-04-27 14:52:44 -0400

    feat: Add functionality to NewPost component. Post dummy data to server

--- frontend/src/Components/NewPost/NewPost.js
+++ frontend/src/Components/NewPost/NewPost.js
@@ -1,10 +1,53 @@
 import React, { Component } from "react"
+import Input from "../Layout/Input/Input"
+import Card from "../Layout/Card/Card"
 
 class NewPost extends Component {
+    state = {
+        postForm: {
+            id: "",
+            title: "",
+            body: ""
+        }
+    }
+    submitHandler = (event) => {
+        event.preventDefault();
+        const request = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(this.state.postForm)
+        }
+        fetch("https://jsonplaceholder.typicode.com/posts", request)
+            .then(response => {
+                console.log(response)
+                this.props.history.push('/')
+            })
+            .catch(error => console.log(error))
 
+    }
+    inputChangedHandler = (event, inputId) => {
+        const updatedPostForm = { ...this.state.postForm }
+        updatedPostForm[inputId] = event.target.value
+
+        this.setState({ postForm: updatedPostForm })
+    }
     render() {
         return (
-            <h1>New Post</h1>
+            <Card color="white">
+                <form onSubmit={(event) => this.submitHandler(event)}>
+                    <h5 className="card-header" style={{ backgroundColor: "white" }}>Nuevo Anuncio</h5>
+                    {/* <Input placeholder="Animal" />
+                    <Input placeholder="Sexo" />
+                    <Input placeholder="Edad" />
+                    <Input placeholder="Direccion" />
+                    <Input placeholder="Provincia" />
+                    <Input placeholder="Municipio" />
+                    <Input placeholder="Descripcion" type="textarea" /> */}
+                    <Input config={{ placeholder: "Title", type: "textarea" }} changed={(event) => this.inputChangedHandler(event, "title")} />
+                    <Input config={{ placeholder: "Body", type: "textarea" }} changed={(event) => this.inputChangedHandler(event, "body")} />
+                    <button type="submit">Publicar</button>
+                </form>
+            </Card>
         )
     }
 }
