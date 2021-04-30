2d3b4ba7aeaeaa14970d127cee9a3018718f57ea
Jose Jorge Rodriguez Salgado <josejorgexl@gmail.com>
2021-04-30 18:39:23 -0400

    fix: drop database before all tests

--- backend/test/cleaner.js
+++ backend/test/cleaner.js
@@ -7,21 +7,10 @@ before((done) => {
 
     while(connector.connection == null)
         continue
-        
-    if (connector.connection.collections.users)
-        connector.connection.collections.users.drop(() => {
-            if (connector.connection.collections.alerts)
-                connector.connection.collections.alerts.drop(() => {
-                    if (connector.connection.collections.tokens)
-                        connector.connection.collections.tokens.drop(done)
-                    else
-                        done()
-            })
-            else
-                done()
-        })
-    else
-        done()
+    
+    connector.connection.dropDatabase()
+    
+    done()
 })
 
 after((done) => {
