019a970c148b8fa4a7b7a7a496e84958eccb8157
Amalia Ibarra <amalia.ibarra@estudiantes.matcom.uh.cu>
2021-08-11 19:25:50 -0400

    feat: add about us first version

--- frontend/src/Components/AboutUs/AboutUs.js
+++ frontend/src/Components/AboutUs/AboutUs.js
@@ -1,15 +1,27 @@
 import React, { Fragment } from "react"
 import "./AboutUs.css"
 import TopHeader from "../TopHeader/TopHeader";
+import { GrInstagram } from "react-icons/gr"
+import { ImFacebook2 } from "react-icons/im"
+import { HiOutlineMail } from "react-icons/hi"
 
 function AboutUs() {
     return (
         <Fragment>
-            <TopHeader title="Sobre nosotros" smallTitle="No se que poner aqui" goHome={true} />
+            <TopHeader title="Sobre nosotros" smallTitle="" goHome={true} />
             <div className="mt-3">
                 <div style={{ textAlign: "initial" }}>
                     <p>
-                        PENDIENTE
+                        Bienvenido a Tribilin, sitio web para publicar sobre mascotas vulnerables en Cuba.
+                    </p>
+                    <p>
+                        Nuestra intencion es concentrar toda la informacion dispersa en las redes
+                        sobre mascotas en adopcion, abandonadas, perdidas o en estado critico, para que sea mas facil
+                        encontrarles un hogar.
+                    </p>
+                    <p>
+                        Somos un quipo de tres, conformado por estudiantes y egresados de la Universidad de La Habana
+                        en las carreras de Disenno y Ciencias de la Computacion.
                     </p>
                 </div>
             </div>
@@ -41,8 +53,27 @@ function AboutUs() {
                 </div>
                 <div style={{ textAlign: "initial" }}>
                     <p>
-                        PENDIENTE
+                        Siguenos en nuestras redes sociales y cuentanos que crees de Tribilin.
+                        Tu opinion es importante para nosotros.
                     </p>
+                    <div>
+                        <GrInstagram size="20" />{" "}
+                        <a rel="noreferrer" target="_blank" href="https://www.instagram.com/tribilin_cuba/?hl=es">
+                            @tribilin_cuba
+                        </a>
+                    </div>
+                    <div className="mt-2">
+                        <ImFacebook2 size="19" />{" "}
+                        <a rel="noreferrer" target="_blank" href="https://www.facebook.com/Tribilin-100383072360000">
+                            Tribilin
+                        </a>
+                    </div>
+                    <div className="mt-2">
+                        <HiOutlineMail size="23" />{" "}
+                        <a rel="noreferrer" target="_blank" href="mailto:tribilincuba@gmail.com">
+                            tribilincuba@gmail.com
+                        </a>
+                    </div>
                 </div>
             </div>
         </Fragment>
